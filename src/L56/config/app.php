c7efb9ae26704ce1d04de36c9cc3ac2fbf358cb8
rognoni <rognoni@users.noreply.github.com>
2019-12-23 23:10:57 +0100

    composer require ecrmnn/laravel-https

--- src/L56/config/app.php
+++ src/L56/config/app.php
@@ -146,6 +146,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        \Ecrmnn\LaravelHttps\Providers\ServiceProvider::class,
 
         /*
          * Package Service Providers...
