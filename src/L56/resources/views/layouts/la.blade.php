8f206887e8f2533cddce6a95c333d170d38b0ae6
rognoni <rognoni@users.noreply.github.com>
2019-12-28 21:27:14 +0100

    Added new layout/welcome Blade template

---
+++ src/L56/resources/views/layouts/la.blade.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <title>LaraVista | Laravel on AlterVista</title>
+        <style>
+            BODY {
+                background-image: url("images/la-background.jpg");
+                background-color: #164e69;
+                height: 100%;
+                background-position: center;
+                background-repeat: no-repeat;
+                background-size: cover;
+            }
+
+            .alter {
+                color: white; 
+                background-color: #faa100;
+            }
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="https://laravista.altervista.org">
+                L<span class="alter">a</span>raVista
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item"><!-- active -->
+                        <!-- a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a -->
+                        <a class="nav-link" href="{{ url('/L56/home') }}">Home</a>
+                    </li>
+                    <!--li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Laravel
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="https://laravel.com/docs">Documentation</a>
+                            <a class="dropdown-item" href="https://laracasts.com">Laracasts</a>
+                            <a class="dropdown-item" href="https://laravel-news.com">News</a>
+                            <a class="dropdown-item" href="https://forge.laravel.com">Forge</a>
+                            <a class="dropdown-item" href="https://github.com/laravel/laravel">GitHub</a>
+                        </div>
+                    </li>
+                </ul>
+                <!-- form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form -->
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Anonymous
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('login') }}">Login</a>
+                            <a class="dropdown-item" href="{{ route('register') }}">Register</a>
+                            <!-- div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a -->
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        @yield('content')
+
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </body>
+</html>
