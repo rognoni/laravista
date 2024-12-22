3c238ee152419877d8c6fb3ddfbab5abbed711cd
rognoni <rognoni@users.noreply.github.com>
2024-12-22 15:13:25 +0100

    Added MySQL tables 'prefix' => 'l11x_'

--- src/L11x/config/database.php
+++ src/L11x/config/database.php
@@ -53,7 +53,7 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
+            'prefix' => 'l11x_',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
