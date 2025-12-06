3423e96a11cb6b2055882f80f4c376f9895c20c2
rognoni <rognoni@users.noreply.github.com>
2025-12-06 22:15:57 +0100

    [L12x] Added .gitignore .env (dev/prod) .htaccess (dev/prod); used "L12x" (database/web)

--- src/L12x/config/database.php
+++ src/L12x/config/database.php
@@ -54,7 +54,7 @@ return [
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
+            'prefix' => 'l12x_',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
--- src/L12x/routes/web.php
+++ src/L12x/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/L12x', function () {
     return view('welcome');
 });
